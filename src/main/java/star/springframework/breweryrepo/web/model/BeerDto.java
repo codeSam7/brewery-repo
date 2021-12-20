1a86882489a213ae2b3d722f9eb9e7e3ceb1cc3d
Ramathirtha Randhi <76068572+codeSam7@users.noreply.github.com>
2021-12-20 11:37:19 -0500

    Added Spring data

--- src/main/java/star/springframework/breweryrepo/web/model/BeerDto.java
+++ src/main/java/star/springframework/breweryrepo/web/model/BeerDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -18,7 +20,15 @@ public class BeerDto {
 
     private UUID id;
     private String beerName;
-    private String beerStyle;
+
     private Long upc;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private BeerStyleEnum beerStyle;
+
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
 
 }
---
+++ src/main/java/star/springframework/breweryrepo/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package star.springframework.breweryrepo.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,PILSNER,STOUT,PALE_ALE,WHEAT,IPA,GOSE,PORTER
+}
